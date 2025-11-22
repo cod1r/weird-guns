8c7fd578bb61bc89d903359333d7e07d0e8a68a5
cod1r <jasonho353@gmail.com>
2025-11-22 06:15:34 -0600

    got bullets coming in with rotation and positioning

--- main.gd
+++ main.gd
@@ -4,15 +4,31 @@ var screen_size
 @export var mouse_mob: PackedScene
 @export var barnacle_mob: PackedScene
 @export var bee_mob: PackedScene
+
+@export var bullet_scene: PackedScene
 const mouse_mob_speed := 400
 const barnacle_mob_speed := 200
 const bee_mob_speed := 250
+var bullet_speed = 400
 var global_mobs: Array[Node2D] = []
+
+var bullets: Array[Dictionary] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	$Player.position = $PlayerPosition.position
+	$Player.z_index = 1
+	$Player/Gun.z_index = 2
+
+func spawn_bullet(trajectory: Vector2) -> void:
+	var new_bullet: Node2D = bullet_scene.instantiate()
+	new_bullet.position = $Player.position
+	new_bullet.z_index = 0
+	new_bullet.name += String.num_int64((get_children()).size())
+	new_bullet.transform = new_bullet.transform.rotated_local(acos(trajectory.x) * sign(trajectory.y))
+	bullets.append({"bullet": new_bullet, "trajectory": trajectory})
+	add_child(new_bullet)
 
 @export var player_speed = 300
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,25 +39,63 @@ func _process(delta: float) -> void:
 		Input.is_action_pressed("s_key"),
 		Input.is_action_pressed("d_key")
 	]
+
 	var w = keys[0]; var a = keys[1]; var s = keys[2]; var d = keys[3];
+
 	var velocity = Vector2.ZERO
+
 	if (w || a || s || d):
-		$Player/AnimatedSprite2D.animation = "character_walk"
-		$Player/AnimatedSprite2D.play()
+		$Player/Body.animation = "character_walk"
+		$Player/Body.play()
 	else:
-		$Player/AnimatedSprite2D.stop()
+		$Player/Body.stop()
 
 	if (w): velocity.y -= 1
 	if (a): velocity.x -= 1
 	if (s): velocity.y += 1
 	if (d): velocity.x += 1
 
-	$Player.position += velocity * player_speed * delta
+	var vel_norm = velocity.normalized()
+
+	$Player.position += vel_norm * player_speed * delta
 	$Player.position = $Player.position.clamp(Vector2.ZERO, screen_size)
 
-	$Player/AnimatedSprite2D.flip_h = velocity.x < 0
+	$Player/Body.flip_h = velocity.x < 0
+
+	update_gun_position()
+
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		var mouse_position = get_viewport().get_mouse_position()
+		spawn_bullet((mouse_position - $Player.position).normalized())
 
 	update_mob_positions(delta)
+
+	for dict in bullets:
+		var bullet = dict["bullet"]
+		var trajectory = dict["trajectory"]
+		bullet.position += trajectory * delta * bullet_speed
+
+	var viewport = get_viewport_rect()
+	bullets = bullets.filter(func(dict: Dictionary):
+		var encloses = viewport.encloses(dict["bullet"].get_viewport_rect())
+		if not encloses:
+			remove_child(dict["bullet"])
+		return encloses)
+
+
+func update_gun_position():
+	var mouse_position: Vector2 = get_viewport().get_mouse_position()
+	var gun: Sprite2D = $Player/Gun
+
+	var diff_vec: Vector2 = (mouse_position - $Player.position).normalized()
+
+	var current_rotation = gun.transform.get_rotation()
+	var diff_rotation = acos(diff_vec.x) * sign(diff_vec.y) - current_rotation
+	gun.transform = gun.transform.rotated(diff_rotation)
+	var current_gun_translation = gun.transform.get_origin()
+	var diff_translation = diff_vec * 50 - current_gun_translation
+	gun.transform = gun.transform.translated(diff_translation)
+	gun.flip_v = gun.position.x < 0
 
 func update_mob_positions(delta: float):
 	var player_position := $Player.position as Vector2
@@ -58,11 +112,9 @@ func update_mob_positions(delta: float):
 	for i in range(global_mobs.size()):
 		var key = global_mobs[i].name.get_slice("Mob", 0)
 
-		global_mobs[i].get_child(0).flip_h = \
-			directions[i].x > 0
+		global_mobs[i].get_child(0).flip_h = directions[i].x > 0
 
-		global_mobs[i].position += \
-			(speeds[key] * directions[i] * delta) as Vector2
+		global_mobs[i].position += (speeds[key] * directions[i] * delta) as Vector2
 
 func _on_mob_mob_spawn() -> void:
 	var mobs := [bee_mob, barnacle_mob, mouse_mob] as Array[PackedScene]
